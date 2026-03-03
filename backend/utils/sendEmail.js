9c441657be7b6dcce552f6a1c971e0efd028b6c5
Saif Mahmud <saif287472@gmail.com>
2026-03-03 09:21:57 +0600

    Switching to resend

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,27 +1,14 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, html) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    await transporter.sendMail({
-      from: `"EarnMinute" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      html,
-    });
-
-    console.log("Email sent successfully");
-  } catch (error) {
-    console.error("EMAIL ERROR:", error);
-    throw new Error("Email sending failed");
-  }
+  await resend.emails.send({
+    from: process.env.EMAIL_FROM,
+    to,
+    subject,
+    html,
+  });
 };
 
 module.exports = sendEmail;
