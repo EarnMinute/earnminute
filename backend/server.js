3cdadc62dbeb990692d55d9f41c8067c5776a4fa
Saif Mahmud <saif287472@gmail.com>
2026-03-03 12:39:48 +0600

    Set up local test env

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.options("*", cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://earnminute.vercel.app");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
   next();
 });
 
@@ -39,7 +39,7 @@ app.use(async (req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("TaskForce BD API is running 🚀");
+  res.send("EarnMinute API is running 🚀");
 });
 
 app.use("/api/v1/auth", authRoutes);
