b4eb8e6de03c933597b43b18c76e535b86ca2898
Saif Mahmud <saif287472@gmail.com>
2026-03-03 11:53:03 +0600

    Fix CORS to allow PATCH

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(cors({
   origin: "https://earnminute.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
